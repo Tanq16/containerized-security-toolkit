98dcb7af6dc3c611e43359e5594469c16fb976fa
Tanishq Rupaal <tanishq.rupaal@praetorian.com>
2022-12-06 19:02:31 -0600

    fix workflow_create [no ci]

--- workflow_structure_create.sh
+++ workflow_structure_create.sh
@@ -3,7 +3,7 @@
 mkdir -p $HOME/docker_work/persist
 
 echo '#!/bin/zsh' > $HOME/docker_work/run.sh
-echo 'sed -i "s/autoload -Uz bracketed-paste-magic/#autoload -Uz bracketed-paste-magic/" ~/.oh-my-zsh/lib/misc.zsh' > $HOME/docker_work/run.sh
-echo 'sed -i "s/zle -N bracketed-paste bracketed-paste-magic/#zle -N bracketed-paste bracketed-paste-magic/" ~/.oh-my-zsh/lib/misc.zsh ' > $HOME/docker_work/run.sh
-echo 'sed -i "s/autoload -Uz url-quote-magic/#autoload -Uz url-quote-magic/" ~/.oh-my-zsh/lib/misc.zsh ' > $HOME/docker_work/run.sh
-echo 'sed -i "s/zle -N self-insert url-quote-magic/#zle -N self-insert url-quote-magic/" ~/.oh-my-zsh/lib/misc.zsh ' > $HOME/docker_work/run.sh
+echo 'sed -i "s/autoload -Uz bracketed-paste-magic/#autoload -Uz bracketed-paste-magic/" ~/.oh-my-zsh/lib/misc.zsh' >> $HOME/docker_work/run.sh
+echo 'sed -i "s/zle -N bracketed-paste bracketed-paste-magic/#zle -N bracketed-paste bracketed-paste-magic/" ~/.oh-my-zsh/lib/misc.zsh ' >> $HOME/docker_work/run.sh
+echo 'sed -i "s/autoload -Uz url-quote-magic/#autoload -Uz url-quote-magic/" ~/.oh-my-zsh/lib/misc.zsh ' >> $HOME/docker_work/run.sh
+echo 'sed -i "s/zle -N self-insert url-quote-magic/#zle -N self-insert url-quote-magic/" ~/.oh-my-zsh/lib/misc.zsh ' >> $HOME/docker_work/run.sh
