176dc2ad24df8a462baad6eaf77f6a7b5415b38c
Tanishq Rupaal <tanishq.rupaal@praetorian.com>
2023-06-16 20:57:33 -0500

    fix script

--- ec2-builder-script.sh
+++ ec2-builder-script.sh
@@ -5,7 +5,7 @@ git clone https://github.com/tanq16/containerized-security-toolkit --depth 1
 cd containerized-security-toolkit/security_docker
 DOCKER_BUILDKIT=1 docker build -f builder.Dockerfile -t builder .
 docker builder prune -f
-docker run -v $PWD:/shared --rm -it builder 'mv executables/ neovim-linux64.deb /shared/'
+docker run -v $PWD:/shared --rm -it builder sh -c 'mv executables/ neovim-linux64.deb /shared/'
 DOCKER_BUILDKIT=1 docker build -f Dockerfile -t tanq16/sec_docker:main .
 # DOCKER_BUILDKIT=1 docker build -f Dockerfile.AppleSilicon -t tanq16/sec_docker:main_apple .
 docker login --username $1 --password $2
