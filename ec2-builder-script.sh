99fe5c8d5b5eabbfcd453298ed2e9aeb867ca33d
Tanishq Rupaal <tanishq.rupaal@praetorian.com>
2023-06-16 20:00:46 -0500

    update script

--- ec2-builder-script.sh
+++ ec2-builder-script.sh
@@ -1,6 +1,6 @@
 apt update -y && apt install docker.io -y
 git clone https://github.com/tanq16/containerized-security-toolkit --depth 1
-cd security_docker
+cd containerized-security-toolkit/security_docker
 DOCKER_BUILDKIT=1 docker build -f builder.Dockerfile -t builder .
 docker builder prune -f
 docker run -v $PWD:/shared --rm -it test 'mv /executables/ /neovim-linux64.deb /shared/'
