721d6cb9e3147c8136fb59a9f1ad47c58e47e653
Tanishq Rupaal <tanishq.rupaal@praetorian.com>
2023-06-16 19:58:34 -0500

    script update - add builder prune

--- ec2-builder-script.sh
+++ ec2-builder-script.sh
@@ -2,6 +2,7 @@ apt update -y && apt install docker.io -y
 git clone https://github.com/tanq16/containerized-security-toolkit --depth 1
 cd security_docker
 DOCKER_BUILDKIT=1 docker build -f builder.Dockerfile -t builder .
+docker builder prune -f
 docker run -v $PWD:/shared --rm -it test 'mv /executables/ /neovim-linux64.deb /shared/'
 DOCKER_BUILDKIT=1 docker build -f Dockerfile -t tanq16/sec_docker:main .
 # DOCKER_BUILDKIT=1 docker build -f Dockerfile.AppleSilicon -t tanq16/sec_docker:main_apple .
