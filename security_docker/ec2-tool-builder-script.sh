f0a2e2ca9f2519d098112e96b8e002cec34316e0
Tanishq Rupaal <tanishq.rupaal@praetorian.com>
2023-09-05 20:33:59 -0500

    docker install proper

--- security_docker/ec2-tool-builder-script.sh
+++ security_docker/ec2-tool-builder-script.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 # init
-apt update -y 2>/dev/null 1>/dev/null && apt install docker.io -y 2>/dev/null 1>/dev/null
+# apt update -y 2>/dev/null 1>/dev/null && apt install docker.io -y 2>/dev/null 1>/dev/null
+apt install ca-certificates curl gnupg -y 2>/dev/null && install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg && echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update -y && apt install install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
 git clone https://github.com/tanq16/containerized-security-toolkit --depth 1 2>/dev/null 1>/dev/null
 cd containerized-security-toolkit/security_docker
 
