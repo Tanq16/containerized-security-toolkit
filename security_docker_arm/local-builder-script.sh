d570285d85fde0d5302f28ea2832c923fafbf15c
Tanishq Rupaal <tanishq.rupaal@praetorian.com>
2024-03-02 14:37:14 -0600

    setup arm dockerfile for latest [no ci]

--- security_docker_arm/local-builder-script.sh
+++ security_docker_arm/local-builder-script.sh
@@ -1,5 +1,4 @@
-DOCKER_BUILDKIT=1 docker build -f builder-goexecs.Dockerfile -t gobuilder .
-DOCKER_BUILDKIT=1 docker build -f builder-others.Dockerfile -t otherbuilder .
+DOCKER_BUILDKIT=1 docker build -f builder.Dockerfile -t intermediate_builder .
 docker builder prune -f
 DOCKER_BUILDKIT=1 docker build -t tanq16/sec_docker:main_apple .
 docker push tanq16/sec_docker:main_apple
