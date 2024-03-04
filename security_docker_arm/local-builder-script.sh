d7bc7f4d65443724c277e4b569a7eeb462a651e8
Tanishq Rupaal <tanishq.rupaal@praetorian.com>
2024-03-03 20:19:26 -0600

    fix ci [no ci]

--- security_docker_arm/local-builder-script.sh
+++ security_docker_arm/local-builder-script.sh
@@ -1,4 +1,4 @@
-DOCKER_BUILDKIT=1 docker build -f builder.Dockerfile -t intermediate_builder .
+DOCKER_BUILDKIT=1 docker buildx build --platform linux/arm64 -f builder.Dockerfile -t intermediate_builder .
 docker builder prune -f
-DOCKER_BUILDKIT=1 docker build -t tanq16/sec_docker:main_apple .
+DOCKER_BUILDKIT=1 docker buildx build --platform linux/arm64 -t tanq16/sec_docker:main_apple .
 docker push tanq16/sec_docker:main_apple
