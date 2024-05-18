0cb8ab7bd4e4a677a759d5f8d9e8c8424196d135
Tanishq Rupaal <tanishq.rupaal@praetorian.com>
2024-05-17 22:34:07 -0500

    fix test 4 [no ci]

--- security_docker_arm/local-builder-script.sh
+++ security_docker_arm/local-builder-script.sh
@@ -1,5 +1,5 @@
-DOCKER_BUILDKIT=1 docker buildx build --platform=linux/arm64 -f builder.Dockerfile -t tanq16/intermediate_builder:arm .
+DOCKER_BUILDKIT=1 docker buildx build -o type=docker --platform=linux/arm64 -f builder.Dockerfile -t tanq16/intermediate_builder:arm .
 docker push tanq16/intermediate_builder:arm
 docker builder prune -f
-DOCKER_BUILDKIT=1 docker buildx build --platform=linux/arm64 -t tanq16/sec_docker:main_apple .
+DOCKER_BUILDKIT=1 docker buildx build -o type=docker --platform=linux/arm64 -t tanq16/sec_docker:main_apple .
 docker push tanq16/sec_docker:main_apple
