6d500e7bbbeb00fbe7990b5f01cec817f37af116
Tanishq Rupaal <tanishq.rupaal@praetorian.com>
2024-05-17 20:34:39 -0500

    push intermediate to hub too

--- security_docker_main/local-builder-script.sh
+++ security_docker_main/local-builder-script.sh
@@ -1,4 +1,5 @@
-DOCKER_BUILDKIT=1 docker build -f builder.Dockerfile -t intermediate_builder .
+DOCKER_BUILDKIT=1 docker build -f builder.Dockerfile -t tanq16/intermediate_builder:amd .
 docker builder prune -f
+docker push tanq16/intermediate_builder:amd
 DOCKER_BUILDKIT=1 docker build -t tanq16/sec_docker:main .
 docker push tanq16/sec_docker:main
