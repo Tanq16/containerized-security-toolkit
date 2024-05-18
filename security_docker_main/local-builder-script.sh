28f09eaebfa63dccf6b349678fa6b8c94e32dac5
Tanishq Rupaal <tanishq.rupaal@praetorian.com>
2024-05-18 02:47:38 -0500

    fix runner settings [no ci]

--- security_docker_main/local-builder-script.sh
+++ security_docker_main/local-builder-script.sh
@@ -1,5 +1,4 @@
-DOCKER_BUILDKIT=1 docker build -f builder.Dockerfile -t tanq16/intermediate_builder:amd .
+DOCKER_BUILDKIT=1 docker build -f builder.Dockerfile -t intermediate_builder .
 docker builder prune -f
-docker push tanq16/intermediate_builder:amd
 DOCKER_BUILDKIT=1 docker build -t tanq16/sec_docker:main .
 docker push tanq16/sec_docker:main
