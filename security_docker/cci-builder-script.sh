eda1113b3f71d7f11092909eeb6fd1b937dacd81
Tanishq Rupaal <tanishqrupaal@Tanishqs-MacBook-Pro.local>
2024-12-08 02:47:55 -0500

    fix builder script

--- security_docker/cci-builder-script.sh
+++ security_docker/cci-builder-script.sh
@@ -2,3 +2,4 @@ tag=$1
 DOCKER_BUILDKIT=1 docker build -f builder.Dockerfile -t intermediate_builder .
 docker builder prune -f
 DOCKER_BUILDKIT=1 docker build -t tanq16/sec_docker:$tag .
+docker push tanq16/sec_docker:$tag
