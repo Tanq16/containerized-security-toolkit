df6ceb6274723489293c97f527487db23ec64dbb
Tanishq Rupaal <tanishqrupaal@Tanishqs-MacBook-Pro.local>
2024-12-08 23:42:43 -0500

    readme update; remove deprecated files; rebuild

--- DEPRECATED/cci-builder-script.sh
+++
@@ -1,5 +0,0 @@
-tag=$1
-DOCKER_BUILDKIT=1 docker build -f builder.Dockerfile -t intermediate_builder .
-docker builder prune -f
-DOCKER_BUILDKIT=1 docker build -t tanq16/sec_docker:$tag .
-docker push tanq16/sec_docker:$tag
--- DEPRECATED/security_docker_amd/local-builder-script.sh
+++
@@ -1,4 +0,0 @@
-DOCKER_BUILDKIT=1 docker build -f builder.Dockerfile -t intermediate_builder .
-docker builder prune -f
-DOCKER_BUILDKIT=1 docker build -t tanq16/sec_docker:main .
-docker push tanq16/sec_docker:main
